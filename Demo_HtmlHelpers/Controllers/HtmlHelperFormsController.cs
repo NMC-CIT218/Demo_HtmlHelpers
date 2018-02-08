f446b3bed6477e3589673813d564bd10b7c44ee1
John Velis <john@johnvelis.com>
2018-02-08 12:47:21 -0500

    init push

---
+++ Demo_HtmlHelpers/Controllers/HtmlHelperFormsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Demo_HtmlHelpers.Controllers
+{
+    public class HtmlHelperFormsController : Controller
+    {
+        // GET: HtmlHelperForms
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(FormCollection form)
+        {
+            foreach (var data in form)
+            {
+                ViewBag.Data = data.;
+            }
+
+            return View("DisplayInfo");
+        }
+    }
+}
